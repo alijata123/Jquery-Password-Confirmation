639368bf00fed6bbab3c37bc9c81fc2c7b8f1b70
alijata123 <alijataaa@gmail.com>
2017-01-13 17:43:10 +0200

    Project files added to this repository,

---
+++ app.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Jata on 1.1.2017.
+ */
+
+$("form span").hide();
+
+$("#password").keyup(errorMessageEvent).keyUp(enableButton);
+$("#confirm_password").keyup(matchPass).keyUp(enableButton);
+
+function isPassValid() {
+    return $("#password").val().length > 6;
+    
+}
+
+function isPassMatching() {
+         return $("#confirm_password").val() === $("#password").val();
+}
+
+
+function isButtonEnabled() {
+    return isPassValid() && isPassMatching();
+    
+}
+
+
+function errorMessageEvent() {
+    if (isPassValid()){
+        $(this).next().hide();
+    }
+    else {
+        $(this).next().show();
+    }
+}
+
+
+function matchPass() {
+    if (isPassMatching()) {
+        $(this).next().hide();
+    }
+    else {
+        $(this).next().show();
+    }
+}
+
+function enableButton() {
+    $("#submit").prop("disabled", !isButtonEnabled);
+    if (!isButtonEnabled){
+        $("#submit").css({backgroundColor: "#d3d3d3", color: "#000"});
+    }
+    else {
+        $("#submit").css({backgroundColor: "#2f558e", color: "#FFF"});
+    }
+
+}
